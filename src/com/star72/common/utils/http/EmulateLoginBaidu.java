ed30c6db761c3328777116bdf24ad7180d021123
nextflower <dv3333@163.com>
2014-12-24 22:04:02 +0800

    完成自动登录+提交

--- src/com/star72/common/utils/http/EmulateLoginBaidu.java
+++ src/com/star72/common/utils/http/EmulateLoginBaidu.java
@@ -26,27 +26,19 @@ package com.star72.common.utils.http;
  */
 
 //import java.io.IOException;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
-//import java.util.Calendar;
-//import java.util.Date;
-//import java.util.GregorianCalendar;
 import java.util.HashMap;
-//import java.util.Hashtable;
+import java.util.HashSet;
 import java.util.List;
-//import java.util.Map;
-import java.util.Scanner;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.cookie.Cookie;
-//import org.apache.http.impl.cookie.BasicClientCookie;
-//import org.apache.http.impl.cookie.BasicClientCookie2;
 import org.apache.http.message.BasicNameValuePair;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -62,6 +54,8 @@ import org.jsoup.select.Elements;
 public class EmulateLoginBaidu {
 
 	static crifanLib crl;
+	
+	static String CONTENT = "前排";
 
 	/**
 	 * @param args
@@ -107,50 +101,125 @@ public class EmulateLoginBaidu {
 		boolean flag = validateLoginSuccess();
         
 		if(flag) {
+			Map<String, String> map = new HashMap<String, String>();
+			
 	        String html = crl.getUrlRespHtml("http://tieba.baidu.com/f/like/mylike");
 	        Document doc = Jsoup.parse(html);
-	        Elements tableEs = doc.getElementsByTag("table");
-	        if(tableEs != null && tableEs.size() > 0) {
-	        	Element tableE = tableEs.get(0);
-	        	Elements trEs = tableE.getElementsByTag("tr");
-	        	for(int i=0; i<trEs.size(); i++) {
-	        		if(i > 0) {
-	        			Element trE = trEs.get(i);
-	        			Elements tdEs = trE.getElementsByTag("td");
-	        			if(tdEs != null && tdEs.size() == 4) {
-	        				Element tdE = tdEs.get(0);
-	        				Elements aEs = tdE.getElementsByTag("a");
-	        				if(aEs != null && aEs.size() == 1) {
-	        					Element aE = aEs.get(0);
-	        					String title = aE.attr("title");
-	        					String href = aE.attr("href");
-	        					enterToTieba(title, href);
-	        					
-	        					//TODO 后续打开
-	        					break;
-	        				}
-	        			}
-	        		}
+	        
+	        //j_pagebar
+	        Element pageE = doc.getElementById("j_pagebar");
+	        
+	        Set<String> pageSet = new HashSet<String>();
+	        pageSet.add("http://tieba.baidu.com/f/like/mylike");
+	        
+	        Elements pageAEs = pageE.getElementsByTag("a");
+	        if(pageAEs != null && pageAEs.size() > 0) {
+	        	for(Element e : pageAEs) {
+	        		pageSet.add("http://tieba.baidu.com" + e.attr("href"));
 	        	}
 	        }
 	        
-	        //page 
+	        for(String pageUrl : pageSet) {
+	        	String pageHtml = crl.getUrlRespHtml(pageUrl);
+	        	Document pageDoc = Jsoup.parse(pageHtml);
+	        	Elements tableEs = pageDoc.getElementsByTag("table");
+		        if(tableEs != null && tableEs.size() > 0) {
+		        	Element tableE = tableEs.get(0);
+		        	Elements trEs = tableE.getElementsByTag("tr");
+		        	for(int i=0; i<trEs.size(); i++) {
+		        		if(i > 0) {
+		        			Element trE = trEs.get(i);
+		        			Elements tdEs = trE.getElementsByTag("td");
+		        			if(tdEs != null && tdEs.size() == 4) {
+		        				Element tdE = tdEs.get(0);
+		        				Elements aEs = tdE.getElementsByTag("a");
+		        				if(aEs != null && aEs.size() == 1) {
+		        					Element aE = aEs.get(0);
+		        					String title = aE.attr("title");
+		        					String href = aE.attr("href");
+		        					map.put(title, href);
+		        				}
+		        			}
+		        		}
+		        	}
+		        }
+	        }
+	        
+	        //循环三次
+	        for(int i=0; i<3; i++) {
+	        	for(String key : map.keySet()) {
+		        	enterToTieba(key, map.get(key));
+		        	//一分钟间隔
+		        	try {
+						Thread.sleep(60 * 1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+		        }
+	        }
+	        
 		}
         
 		return ;
 	}
 	
-	private static void enterToTiezi(String url) {
+	private static void enterToTiezi(String url, String biebaTitle, String tieziTitle) {
 		
-		List<NameValuePair> postDict = new ArrayList<NameValuePair>();
-		postDict.add(new BasicNameValuePair("u", "https://passport.baidu.com/"));
-		postDict.add(new BasicNameValuePair("tpl", "mn"));
-		postDict.add(new BasicNameValuePair("staticpage", "https://passport.baidu.com/static/passpc-account/html/v3Jump.html"));
-		postDict.add(new BasicNameValuePair("isPhone", "false"));
-		postDict.add(new BasicNameValuePair("charset", "UTF-8"));
-		postDict.add(new BasicNameValuePair("callback", "parent.bd__pcbs__ra48vi"));
+		System.out.println(biebaTitle + "---" + tieziTitle);
+		
+		String kw = biebaTitle;
+		String content = CONTENT;
+		String fid = null;
+		String tbs = null;
+		String tid = FilenameUtils.getBaseName(url);
+		
+		
+		Pattern tbsP = Pattern.compile("\"tbs\"  : \"(\\w{10,40}+)\"");
+		Pattern fidP = Pattern.compile("fid: '(\\d{1,20})'");
 		
 		String html = crl.getUrlRespHtml(url);
+		
+		Matcher tbsMatcher = tbsP.matcher(html);
+		Matcher fidMatcher = fidP.matcher(html);
+		
+		
+		//"tbs"  : "a88d9a0dd913895e1419421210"
+		if(tbsMatcher.find()) {
+			if(tbsMatcher.groupCount()>0) {
+				tbs = tbsMatcher.group(1);
+			}
+		}
+		
+		//fid: '173802',
+		if(fidMatcher.find()) {
+			if(fidMatcher.groupCount()>0) {
+				fid = fidMatcher.group(1);
+			}
+		}
+		
+		if(StringUtils.isNotBlank(kw) && StringUtils.isNotBlank(fid) && StringUtils.isNotBlank(tbs) && StringUtils.isNotBlank(tid)) {
+			
+			List<NameValuePair> postDict = new ArrayList<NameValuePair>();
+			postDict.add(new BasicNameValuePair("__type__", "reply"));
+			postDict.add(new BasicNameValuePair("content", content));
+			postDict.add(new BasicNameValuePair("ie", "utf-8"));
+			postDict.add(new BasicNameValuePair("kw", kw));
+			postDict.add(new BasicNameValuePair("fid", fid));
+			postDict.add(new BasicNameValuePair("tbs", tbs));
+			postDict.add(new BasicNameValuePair("tid", tid));
+			
+			List<NameValuePair> headerDict = new ArrayList<NameValuePair>();
+			headerDict.add(new BasicNameValuePair("Accept", "application/json, text/javascript, */*; q=0.01"));
+			headerDict.add(new BasicNameValuePair("Accept-Encoding", "gzip, deflate"));
+			headerDict.add(new BasicNameValuePair("Accept-Language", "zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3"));
+			headerDict.add(new BasicNameValuePair("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36"));
+			headerDict.add(new BasicNameValuePair("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8"));
+			headerDict.add(new BasicNameValuePair("Connection", "	keep-alive"));
+			headerDict.add(new BasicNameValuePair("Referer", url));
+			
+			String info = crl.getUrlRespHtml("http://tieba.baidu.com/f/commit/post/add", headerDict, postDict);
+			
+		}
 		
 	}
 
@@ -181,10 +250,9 @@ public class EmulateLoginBaidu {
 				if(numberMatcher.find()) {
 					int repCount = Integer.parseInt(text);
 					//TODO 仅针对前排
-					if(repCount == 0) {
+					if(repCount <= 3) {
 						Elements threadlist_li_rightEs = threadlist_li_leftE.parent().getElementsByClass("threadlist_li_right");
 						if(threadlist_li_rightEs != null) {
-							boolean isOver = false;
 							for(Element threadlist_li_rightE : threadlist_li_rightEs) {
 								//j_th_tit
 								Elements biaotiAEs = threadlist_li_rightE.getElementsByClass("j_th_tit");
@@ -193,23 +261,17 @@ public class EmulateLoginBaidu {
 										String tagName = e.tagName();
 										if("a".equalsIgnoreCase(tagName)) {
 											String tieziHref = e.attr("href");
-											enterToTiezi(url + tieziHref);
-											System.out.println(url + tieziHref);
-											//TODO 
-											isOver = true;
-											break;
+											enterToTiezi(url + tieziHref, title, e.attr("title"));
+											return;
 										}
 											
 									}
-								}
-								
-								if(isOver) {
-									break;
 								}
 							}
 						}
 					}
 				}
+				
 			}
 		}
 		

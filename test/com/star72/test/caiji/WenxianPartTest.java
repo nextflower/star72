96bbaf0f1fa51b88a56086a19ae428ee8d08e2a1
nextflower <dv3333@163.com>
2015-01-15 23:47:36 +0800

    201501152347

--- test/com/star72/test/caiji/WenxianPartTest.java
+++ test/com/star72/test/caiji/WenxianPartTest.java
@@ -8,6 +8,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
@@ -20,79 +22,86 @@ public class WenxianPartTest {
 
 	@Test
 	public void test() throws IOException {
-		String path = "D:/TEST/star/全唐五代词.txt";
+		String path = "D:/temp/綴白裘.txt";
+		String muluPath = "D:/temp/目录.txt";
 		String destPath = FilenameUtils.getFullPath(path) + FilenameUtils.getBaseName(path);
 		
 		List<String> lines = FileUtils.readLines(new File(path));
+		List<String> muluList = FileUtils.readLines(new File(muluPath));
+		
+		//初始化目录
+		Map<String, Set<String>> map = new HashMap<String, Set<String>>();
+		for(String line : muluList) {
+			String[] arr = line.split("：");
+			if(arr.length == 2) {
+				String key = arr[0];
+				String value = arr[1];
+				Set<String> set = map.get(key);
+				if(set == null) {
+					set = new HashSet<String>();
+					map.put(key, set);
+				}
+				String[] split = value.split("、");
+				for(String s : split) {
+					set.add(s);
+				}
+			}
+		}
 		
 		//稿件容器
 		List<String> contentList = new ArrayList<String>();
 		//标题变量
-		String preTitle = null;
 		String title = null;
-		String author = null;
+		String author = "";
+		String t1 = null;
 		
 		int count = 0;
 		for(String line : lines) {
 			
-			if(isAuthor(line)) {
-				author = line;
+			if(StringUtils.isNotBlank(line) && line.length() < 4) {
+				for(String key : map.keySet()) {
+					if(line.equals(key)) {
+						t1 = key;
+						break;
+					}
+				}
 			}
 			
-			if(isTitle(line)) {
+			if(line.equals(t1)) {
+				continue;
+			}
+			
+			Set<String> set = map.get(t1);
+			if(set != null && isTitle(line, set)) {
 				
 				if(title != null) {
-						
+					FileUtils.writeLines(new File(destPath + File.separator + title + ".txt"), contentList);
 				}
 				
-				if(author == null) {
-					author = "佚名";
-				}
-				
-				if(line.contains("【又】") || line.contains("【其")) {
-					
-				}
-				
-				title = ++count + "-" + line + "-唐-" + author;
+				title = ++count + "-" + t1 + " " + line;
+				contentList.clear();
 			} else {
 				contentList.add(line);
 			}
 			
-			
 		}
 		
 		if(title != null) {
-			//FileUtils.writeLines(new File(destPath + File.separator + title + ".txt"), contentList);
+			FileUtils.writeLines(new File(destPath + File.separator + title + ".txt"), contentList);
 		}
 		
 		System.out.println(count);
 	}
 
-	private boolean isTitle(String line) {
-		if(StringUtils.isBlank(line)) {
+	private boolean isTitle(String line, Set<String> set) {
+		if(StringUtils.isBlank(line) || line.length() > 3) {
 			return false;
 		}
-		
-		if(!line.startsWith("　") && !line.startsWith(" ")) {
-			return true;
+		for(String s : set) {
+			if(s.equals(line)) {
+				return true;
+			}
 		}
-		
-		if(line.contains("【")) {
-			return true;
-		}
-		
-		return false;
-	}
-	
-	private boolean isAuthor(String line) {
-		if(StringUtils.isBlank(line)) {
-			return false;
-		}
-		
-		if(!line.startsWith("　") && !line.startsWith(" ")) {
-			return true;
-		}
-		
 		return false;
 	}
 
